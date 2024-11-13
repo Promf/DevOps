a5e9240731dbb21add8fb30c5ff11ec90aacd335
Promf <arsenyover@gmail.com>
2024-11-13 22:45:19 +0300

    main2

--- my/settings.py
+++ my/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib import admin
+from django.urls import path, include
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather_app',
 ]
 
 MIDDLEWARE = [
