25ad7d879a7833e8996fd8a4a9fe702c748d2f8a
Promf <arsenyover@gmail.com>
2024-12-11 23:47:36 +0300

    ef

--- app/tests.py
+++ app/tests.py
@@ -1,25 +1,15 @@
 from django.test import TestCase
-
-# Create your tests here.
-from django.test import TestCase
 from django.urls import reverse
 
-# Пример теста для модели (если у вас есть модель TestModel)
-# class TestModelTest(TestCase):
-#     def test_model_creation(self):
-#         instance = TestModel.objects.create(name="Test Instance")
-#         self.assertEqual(instance.name, "Test Instance")
-
-# Пример теста для представления (view)
 class HomePageTest(TestCase):
     def test_home_page_status_code(self):
-        response = self.client.get(reverse('home')) # 'home' - имя вашего URL
+        response = self.client.get(reverse('weather_app:index')) # 'weather_app:index',  app_name
         self.assertEqual(response.status_code, 200)
 
     def test_home_page_contains_correct_html(self):
-        response = self.client.get(reverse('home'))
-        self.assertContains(response, '<h1>Welcome</h1>') # Проверяем наличие заголовка
+        response = self.client.get(reverse('weather_app:index'))
+        self.assertContains(response, "<h1>Weather App</h1>")  # Замените на ожидаемый HTML
 
     def test_home_page_does_not_contain_incorrect_html(self):
-        response = self.client.get(reverse('home'))
-        self.assertNotContains(response, 'Hi there!')
+        response = self.client.get(reverse('weather_app:index'))
+        self.assertNotContains(response, "<h1>Error Page</h1>") # Замените на некорректный HTML
--- weather_app/urls.py
+++ weather_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'weather_app'  # Добавлено app_name
+
+
 urlpatterns = [
-    path('', views.index),  #the path for our index view
+    path('', views.index, name='index'),  #the path for our index view
 ]
